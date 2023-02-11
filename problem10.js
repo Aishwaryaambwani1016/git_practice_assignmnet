74dcaf024ea7bfcf6d7e9c30cae7f95d9047bf67
unknown <kwckhanpur@gmail.com>
2023-02-11 17:23:39 +0530

    palindrome

---
+++ problem10.js
@@ -0,0 +1,13 @@
+// palindrome
+
+let str="Aishwarya";
+let a="";
+   for(let i=str.length-1;i>=0;i--){
+        a=a+str[i];
+    }
+    if(a==str){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
